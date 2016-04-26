3ec0fc1b6bdacd0659ba397fa18aabc9ee34b527
Fabricio C Zuardi <fabricio@fabricio.org>
2016-04-26 16:30:39 -0300

    new bot brain: hello-goodbye

--- index.js
+++ index.js
@@ -19,16 +19,27 @@ const mainDialog = new builder.LuisDialog(LUIS_MARCOPOLO_URL);
 mainDialog.onDefault((session, args) => {
     const urls = [
         `${LUIS_CORTANA_URL}&q=${encodeURIComponent(session.message.text)}`,
-        `${LUIS_ROBOVIARIO_URL}&q=${encodeURIComponent(session.message.text)}`
+        `${LUIS_ROBOVIARIO_URL}&q=${encodeURIComponent(session.message.text)}`,
+        `${LUIS_HELLO_GOODBYE}&q=${encodeURIComponent(session.message.text)}`
     ];
-    Promise.all([rp(urls[0]),rp(urls[1])]).then((values) => {
+    Promise.all([rp(urls[0]), rp(urls[1])]).then((values) => {
         console.log('values', values);
         const cortanaResult = JSON.parse(values[0]);
         const roboviarioResult = JSON.parse(values[1]);
+        const helloGoodbyeResult = JSON.parse(values[2]);
         const message = tsmlj`
-            *calamarcopolo:* ${args.intents[0].intent}, ${args.intents[0].score};
-            *roboviário:* ${roboviarioResult.intents[0].intent}, ${roboviarioResult.intents[0].score};.
-            *cortana:* ${cortanaResult.intents[0].intent}, ${cortanaResult.intents[0].score};
+            *calamarcopolo:*
+                ${args.intents[0].intent},
+                ${args.intents[0].score};
+            *roboviário:*
+                ${roboviarioResult.intents[0].intent},
+                ${roboviarioResult.intents[0].score};.
+            *helloGoodbye:*
+                ${helloGoodbyeResult.intents[0].intent},
+                ${helloGoodbyeResult.intents[0].score};.
+            *cortana:*
+                ${cortanaResult.intents[0].intent},
+                ${cortanaResult.intents[0].score};
         `;
         console.log('message ', message);
         session.send(message);
